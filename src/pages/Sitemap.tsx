0541b2151c033ec6fbd2bd7d97cc9ba38b49952c
Julian Speckmann <176780813+KingIronMan2011@users.noreply.github.com>
2025-06-03 13:38:16 +0200

    Aktualisieren von Sitemap.tsx

--- src/pages/Sitemap.tsx
+++ src/pages/Sitemap.tsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 
 const sitemapLinks = [
   { path: "/", label: "Home" },
+  { path: "/aboutus", label: "About Us" },
+  { path: "/privacy", label: "Privacy Policy" },
   // Add more routes as needed
 ];
 
